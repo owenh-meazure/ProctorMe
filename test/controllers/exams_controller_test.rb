385b39c8296f270502cfc723e39395d236cfbdf7
Owen H <l11e8wnir@relay.firefox.com>
2021-04-26 14:57:43 -0400

    scaffolding for colleges, exams, and users

---
+++ test/controllers/exams_controller_test.rb
@@ -0,0 +1,38 @@
+require "test_helper"
+
+class ExamsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @exam = exams(:one)
+  end
+
+  test "should get index" do
+    get exams_url, as: :json
+    assert_response :success
+  end
+
+  test "should create exam" do
+    assert_difference('Exam.count') do
+      post exams_url, params: { exam: { start_time: @exam.start_time } }, as: :json
+    end
+
+    assert_response 201
+  end
+
+  test "should show exam" do
+    get exam_url(@exam), as: :json
+    assert_response :success
+  end
+
+  test "should update exam" do
+    patch exam_url(@exam), params: { exam: { start_time: @exam.start_time } }, as: :json
+    assert_response 200
+  end
+
+  test "should destroy exam" do
+    assert_difference('Exam.count', -1) do
+      delete exam_url(@exam), as: :json
+    end
+
+    assert_response 204
+  end
+end
